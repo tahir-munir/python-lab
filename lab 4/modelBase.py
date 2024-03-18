f8617951ad60f820b3b1712e6e8bd837240b2aef
tahiraayyy <m.tahirmunir136@gmail.com>
2024-03-18 14:04:30 +0500

    model base

--- lab 4/modelBase.py
+++ lab 4/modelBase.py
@@ -1,11 +1,17 @@
 class ModelBaseAgent():
-    def __init__(self,a, b):
-        self.model = ("loc_a" : a , "Loc_b" : b)
+    def __init__(self,inti_a, inti_b):
+        self.model = {"loc_a" : inti_a , "Loc_b" : inti_b}
     def DoAction(self,location,status):
         self.model[location] = status
         print(self.model)
-        if self.model["Loc_a"] = self.model["Loc_a"] == 'clean'
+        if self.model["Loc_a"] == self.model["Loc_a"] == 'clean'
             return 'NoOp'
         elif status == 'dirty':
             return 'suck'
-        elif location == 'Loca_a'
+        elif location == 'Loca_a':
+            return 'right'
+        else:
+            return 'left'
+        
+a = ModelBaseAgent('dirty','dirty')
+print(a.DoAction('Loc_a', 'dirty'))
