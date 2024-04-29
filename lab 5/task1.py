5259a3aedbd774aeba7cd88898f54ea1c956da46
tahiraayyy <m.tahirmunir136@gmail.com>
2024-04-29 14:50:26 +0500

    lab5

---
+++ lab 5/task1.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+random_array = np.random.rand(10, 4)
+
+first_five_rows = random_array[:5]
+
+print("Original array:")
+print(random_array)
+print("\nFirst five rows:")
+print(first_five_rows)
---
+++ lab 5/task2.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+
+exam_data = {
+    'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+    'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+    'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+    'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']
+}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+df = pd.DataFrame(exam_data, index=labels)
+result = df[df['attempts'] > 2]
+
+print("Number of attempts in the examination is greater than 2:")
+print(result)
+
+
+total_score = df['score'].sum()
+count_valid_scores = df['score'].count()
+average_score = total_score / count_valid_scores
+print("The average score is: " , average_score)
